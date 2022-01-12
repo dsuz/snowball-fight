0dca401e11d88d3973482a94861af3cdc3184e8d
RYUKI <ryukiarashi@icloud.com>
2022-01-12 21:51:15 +0900

    マスターがボタンを押したら対戦開始

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,38 @@
 ﻿using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using System.Linq;
 
 /// <summary>
 /// ゲームを管理する。
 /// 接続 > 部屋に入って以降の管理をする。
 /// </summary>
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour, IOnEventCallback
 {
+    /// </summary>キャラを出現させる位置</summary>
     [SerializeField] Transform[] _spawnPoints = default;
+    /// </summary>生成する物のプレハブ名</summary>
     [SerializeField] string _playerPrefabName = "";
+    /// </summary>ゲーム開始フラグ</summary>
+    [SerializeField] bool _inGame = false;
+
+    /// </summary>制限時間</summary>
+    [SerializeField] float _gameTimer = default;
+    /// </summary>時間を表示するテキスト</summary>
+    [SerializeField] Text _timerText = default;
+    /// </summary>終わる時間</summary>
+    float _time = 0;
+
+    private void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
 
     public void StartGame()
     {
@@ -20,4 +44,25 @@ public class GameManager : MonoBehaviour
         player.name = $"Player{actorNumber}";
         player.GetComponent<PlayerController>().enabled = true;
     }
+
+    void IOnEventCallback.OnEvent(ExitGames.Client.Photon.EventData photonEvent)
+    {
+        switch (photonEvent.Code)
+        {
+            case (byte)NetworkEvents.GameStart:
+                Debug.Log("Game Start");
+                _inGame = true;
+                break;
+            case (byte)NetworkEvents.Die:
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public enum NetworkEvents : byte
+{
+    GameStart,
+    Die,
 }
--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -11,6 +11,8 @@ using UnityEngine.Events;
 /// </summary>
 public class NetworkManager : MonoBehaviourPunCallbacks // Photon Realtime 用のクラスを継承する
 {
+    /// <summary>ゲーム開始ボタン</summary>
+    [SerializeField] GameObject _startButton;
     /// <summary>最大プレイ可能人数</summary>
     [SerializeField] int _maxPlayerCount = 2;
     /// <summary>ゲーム開始時に実行される処理</summary>
@@ -89,10 +91,33 @@ public class NetworkManager : MonoBehaviourPunCallbacks // Photon Realtime 用�
     }
 
     /// <summary>
+    /// buttonに設定
+    /// </summary>
+    public void GameStart()
+    {
+        _startButton.SetActive(false);
+
+        // 対戦中に他が入れないようにする
+        Debug.Log("Closing Room");
+        PhotonNetwork.CurrentRoom.IsOpen = false;
+
+        RaiseEventOptions raiseEventoptions = new RaiseEventOptions();
+        raiseEventoptions.Receivers = ReceiverGroup.All;
+        SendOptions sendOptions = new SendOptions();
+        PhotonNetwork.RaiseEvent((byte)NetworkEvents.GameStart, null, raiseEventoptions, sendOptions);
+    }
+
+    /// <summary>
     /// ゲームを開始する
     /// </summary>
     void StartGame()
     {
+        // マスターだけにボタンを表示
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            _startButton.SetActive(true);
+        }
+
         Debug.Log("Start Game");
         _onGameStart.Invoke();
     }
